f5eb24e9fbf8caa484243f07a406563fc93cb59f
xulianjie@landawang.com <xulianjie@landawang.com>
2019-03-16 10:58:12 +0800

    配置2333

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -105,7 +105,7 @@ export default {
                 return false
             }
             // 登录接口
-            this.$router.push({name:'index'})
+            this.$router.push({name:'gys'})
         },
         handleRegister() {
             this.$router.push({
